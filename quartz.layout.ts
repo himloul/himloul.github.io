7463823134432809c3097acebc2a37f80973218d
himloul <hamzaonde@gmail.com>
2025-03-29 18:12:08 +0000

    Filter folders in explorer
    Commented

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -25,18 +25,29 @@ export const defaultContentPageLayout: PageLayout = {
   left: [
     // Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    /* Component.Explorer({
-      folderClickBehavior: "link",
-      folderDefaultState: "collapsed"
-    }), */
-  ],
-  right: [
     Component.Flex({
       components: [
         { Component: Component.Search(), grow: false, },
         { Component: Component.Darkmode() },
       ],
     }),
+
+    /* Component.Explorer({
+      title: "Explorer",
+      folderClickBehavior: "link",
+      folderDefaultState: "collapsed",
+      useSavedState: true,
+      filterFn: (node) => {
+        if (node.isFolder) {
+          const folderName = node.displayName.toLowerCase(); // or node.data?.slug, adjust based on your folder structure
+          return folderName === "posts" || folderName === "projects";
+        }
+        return false; // Don't show files at the root level
+      },
+      order: ["filter", "map", "sort"], // Make sure "filter" is included in the order
+    }) */
+  ],
+  right: [
     Component.DesktopOnly(Component.TableOfContents()),
     Component.Backlinks(),
     // Component.Graph(),
@@ -53,18 +64,18 @@ export const defaultListPageLayout: PageLayout = {
   left: [
     // Component.PageTitle(),
     Component.MobileOnly(Component.Spacer()),
-    /* Component.Explorer({
-      folderClickBehavior: "link",
-      folderDefaultState: "collapsed"
-    }), */
-  ],
-  right: [
     Component.Flex({
       components: [
         { Component: Component.Search(), grow: false, },
         { Component: Component.Darkmode() },
       ],
     }),
+    /* Component.Explorer({
+      folderClickBehavior: "link",
+      folderDefaultState: "collapsed"
+    }), */
+  ],
+  right: [
     // Component.Graph(),
   ],
 }
